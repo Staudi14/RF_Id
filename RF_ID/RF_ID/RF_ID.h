b857c3166341003f6f3041c6db0c7e0192cc093a
Marie <mlm2001@outlook.de>
2019-05-03 16:34:45 +0200

    Mancheste_Anfang

--- RF_ID/RF_ID/RF_ID.h
+++ RF_ID/RF_ID/RF_ID.h
@@ -2,7 +2,7 @@
 #define RF_ID_HEADER
 
 //Parameters
-#define DATA_LENGTH 8
+#define DATA_LENGTH 7
 
 //PORTB bei Input Pull-up Register !!!!!, sowie nur ein Byte(8Bit)!!!
 //Needed to be changed as the specifications sais that the data must be stored msb first
@@ -17,11 +17,22 @@
     do {                                                                                \
         for (int i = 0; i < DATA_LENGTH; i++)                                           \
         {                                                                               \
-            ret |= ((((1 << i) & tmp) == (1 << i) ? 1 : 0) << (DATA_LENGTH - 1 - i));   \
+            ret |= ((((1 << i) & tmp) == (1 << i) ? 1 : 0) << (DATA_LENGTH - 1 - i));   \  //: vergleich ?wenns true ist setzt er 1 ein wenn es false ist setzt er 0 ein
         }                                                                               \
     } while(0);                                                                         \
 ret;                                                                                    \
 })
 
+//Manchester
+
+#define MANCHESTER_CODIERUNG(daten) ({
+    int i;
+    
+    for(i=0;i<11;i++){
+        
+    }
+
+})
 
 #endif
+
